48d5bf1341b7154e809016c55f250975bb1614fb
Yeem <nayeem90325339@gmail.com>
2021-10-08 02:31:04 -0400

    stored sentence data, start of function to generate msgs

---
+++ mixedmsg.js
@@ -0,0 +1,9 @@
+// data for messages stored in these variables
+const sent1 = ["Ayo,", "Look man,", "Listen here,"]
+const sent2 = ["keep ya head up", "don't worry about all that", "shrug it off"]
+const sent3 = ["and keep it pushing.", "and keep it moving.", "and keep ya chin up."]
+
+// function to generate messages
+const genMsg = (array1,array2,array3) => {
+    console.log(`${sent1[Math.floor(Math.random) *3]}`)
+}
