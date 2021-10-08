d59f5a46fa2bfc749fac7a9f2f29845d188c518f
Yeem <nayeem90325339@gmail.com>
2021-10-08 02:49:14 -0400

    Got the entire project to work as is, effectively done

--- mixedmsg.js
+++ mixedmsg.js
@@ -4,6 +4,7 @@ const sent2 = ["keep ya head up", "don't worry about all that", "shrug it off"]
 const sent3 = ["and keep it pushing.", "and keep it moving.", "and keep ya chin up."]
 
 // function to generate messages
-const genMsg = (array1,array2,array3) => {
-    console.log(`${sent1[Math.floor(Math.random) *3]}`)
+const genMsg = (array1, array2, array3) => {
+    console.log(`${sent1[Math.floor(Math.random() *3)]} ${sent2[Math.floor(Math.random()*3)]} ${sent3[Math.floor(Math.random() *3)]}`)
 }
+genMsg(sent1, sent2, sent3)
